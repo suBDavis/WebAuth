a2504eee565f06757f967b8b1d1d5d08b7e57008
Brandon Davis <bdavis@redspin.net>
2014-12-14 18:33:19 -0500

    derp

--- portal.php
+++ portal.php
@@ -9,22 +9,25 @@ if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect
 //do login info here.
 //On success, set a random cookie and store it in the user table
 //On failure, deflect back to the index page.
+$auth = $_COOKIE["auth"];
+$mc_username = $_POST["mc_username"];
+$mc_pass = $_POST["mc_pass"];
 
 if (isset($_COOKIE['auth'])){
 
-  $sql = "SELECT * FROM `users` WHERE `cookie` LIKE $_COOKIE['auth']";
+  $sql = "SELECT * FROM `users` WHERE `cookie` LIKE $auth";
 
   echo("cookie found");
 
 } else {
 
-  $sql = "SELECT * FROM `users` WHERE `name` LIKE $_POST['mc_username']";
+  $sql = "SELECT * FROM `users` WHERE `name` LIKE $mc_username";
   $result = mysqli_query($con, $sql);
   $row = mysqli_fetch_assoc($result);
 
   echo $row['pass'];
 
-  if ( hash('sha256' , $_POST["mc_pass"]) == $row['pass']){
+  if ( hash('sha256' , $mc_pass) == $row['pass']){
     $c_value =  hash('sha256', time());
     //on success
     setcookie('auth', $c_value);
