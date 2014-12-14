8346a8cb0acd33254f0d15dae2bcf3dd4bf168a2
Brandon Davis <bdavis@redspin.net>
2014-12-14 17:07:21 -0500

    portal

---
+++ portal.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'creds.php';
+
+$con = mysqli_connect("localhost",$sql_user,$sql_password,$sql_db);
+// Check connection
+if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+
+?>
