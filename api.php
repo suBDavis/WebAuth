c902a0d7cad5f68ad4c517b6ad9ec04e62ae2b96
Brandon Davis <bdavis@redspin.net>
2014-12-14 19:24:11 -0500

    derp

--- api.php
+++ api.php
@@ -1,7 +1,20 @@
 <?php
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+  CURLOPT_RETURNTRANSFER => 1,
+  CURLOPT_URL => 'https://api.mojang.com/users/profiles/minecraft/subdavis',
+  CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
 
-$res = curl_init("https://api.mojang.com/users/profiles/minecraft/subdavis");
-$json_a = json_decode($res,true);
+echo $resp;
+// Close request to clear up some resources
+curl_close($curl);
+
+/*$json_a = json_decode($res,true);
 echo $json_a[name];
-echo $json_a[id];
+echo $json_a[id];*/
 ?>
