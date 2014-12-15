2d1042906bb279de9a9f7e22b5b9ef57c1655489
Brandon Davis <bdavis@redspin.net>
2014-12-15 11:39:07 -0500

    profile page:

--- portal.php
+++ portal.php
@@ -49,12 +49,14 @@ if (isset($_COOKIE['auth'])){
       $uuid = getUUID($mc_username);
       $player = $mc_username;
       $isStaff = $row['other'] == 1 ? TRUE : FALSE;
-      //echo $uuid;
       //set cookie in SQL DB
       $sql = "UPDATE `webauth`.`users` SET `cookie` = '".$c_value."' WHERE `users`.`uuid` = '".$uuid."';";
       $result = mysqli_query($con, $sql);
       //do other things?
-
+      //save avitar on the webserver.  Update database with link
+      $url1 = "https://mctoolbox.net/avatar/".$player."/100";
+      $img1 = "avatars/".$player;
+      file_put_contents($img1, file_get_contents($url1));
     } else {
       //do this if login fails
       kickback();
