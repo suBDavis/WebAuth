0a14c917a45be2b78411a66313a2f6b35f1154b3
Brandon Davis <bdavis@redspin.net>
2014-12-15 21:01:42 -0500

    refactor profile update

--- update.php
+++ update.php
@@ -5,14 +5,14 @@ include 'creds.php';
 $con = mysqli_connect("localhost",$sql_user,$sql_password,$sql_db);
 
 if(isset($_POST['prof_email'])){
-  if($_POST['prof_email']) == "")
+  if($_POST['prof_email'] == ""){}
     else{
     $sql = "UPDATE `webauth`.`users` SET `email` = '".$_POST['prof_email']."' WHERE `users`.`uuid` = '".$uuid."';";
     $result = mysqli_query($con, $sql);
   }
 }
 if(isset($_POST['prof_skype'])){
-  if($_POST['prof_skype'] == "")
+  if($_POST['prof_skype'] == ""){}
     else{
     $sql = "UPDATE `webauth`.`users` SET `skype` = '".$_POST['prof_skype']."' WHERE `users`.`uuid` = '".$uuid."';";
     $result = mysqli_query($con, $sql);
